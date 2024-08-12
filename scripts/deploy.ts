51f33601d50487c3392d6a1c265f9df9e091b583
Apexloaded <admin@apexloaded.com>
2024-08-12 21:52:15 +0100

    bill point contract done

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -2,33 +2,61 @@ import { ethers, upgrades, network } from "hardhat";
 
 async function main() {
   const [owner] = await ethers.getSigners();
-  const DexaPay = await ethers.getContractFactory("DexaPay");
-  console.log("Deploying DexaPay...");
-  const dexaPay = await upgrades.deployProxy(DexaPay, [owner.address], {
-    initializer: "init_dexa_pay",
-    initialOwner: owner.address,
-  });
-  await dexaPay.waitForDeployment();
-  const dexaPayAddr = await dexaPay.getAddress();
-  console.log("DexaPay deployed to:", dexaPayAddr);
 
-  const DexaBill = await ethers.getContractFactory("DexaBill");
-  console.log("Deploying DexaBill...");
-  const dexaBill = await upgrades.deployProxy(
-    DexaBill,
-    [owner.address, dexaPayAddr, "https://www.dexapay.xyz"],
+  const entryPoint = await ethers.deployContract("EntryPoint");
+  await entryPoint.waitForDeployment();
+  const epAddress = await entryPoint.getAddress();
+  console.log("EntryPoint deployed to:", epAddress);
+
+
+  const Account = await ethers.getContractFactory("Account");
+  console.log("Deploying Account...");
+  const account = await upgrades.deployProxy(
+    Account,
+    [owner.address, epAddress],
     {
-      initializer: "init_dexa_bill",
+      initializer: "initialize",
       initialOwner: owner.address,
     }
   );
-  await dexaBill.waitForDeployment();
-  const dexaBillAddr = await dexaBill.getAddress();
-  console.log("DexaBill deployed to:", dexaBillAddr);
+  await account.waitForDeployment();
+  const accountAddr = await account.getAddress();
+  console.log("Account deployed to:", accountAddr);
 
-  await dexaPay.init_roles(dexaBillAddr);
-  await dexaBill.init_roles(dexaPayAddr);
-  await dexaPay.batchEnlistTokens([
+
+  const Gateway = await ethers.getContractFactory("Gateway");
+  console.log("Deploying Gateway...");
+  const gateway = await upgrades.deployProxy(
+    Gateway,
+    [owner.address],
+    {
+      initializer: "init_gateway",
+      initialOwner: owner.address,
+    }
+  );
+  await gateway.waitForDeployment();
+  const gatewayAddr = await gateway.getAddress();
+  console.log("Gateway deployed to:", gatewayAddr);
+
+
+  const BillPoint = await ethers.getContractFactory("BillPoint");
+  console.log("Deploying BillPoint...");
+  const billPoint = await upgrades.deployProxy(
+    BillPoint,
+    [owner.address, gatewayAddr, "https://www.dexafi.xyz"],
+    {
+      initializer: "init_bill_point",
+      initialOwner: owner.address,
+    }
+  );
+  await billPoint.waitForDeployment();
+  const billPointAddr = await billPoint.getAddress();
+  console.log("BillPoint deployed to:", billPointAddr);
+
+  
+  await gateway.init_roles(billPointAddr);
+  await billPoint.init_roles(gatewayAddr);
+  await gateway.batchEnlistTokens([
     "0xBf3edC332bd9E1C32D10d2511B61938D1A6b4D01",
     "0xE8a8f500301c778064E380E5bFA9E315a7638134",
   ]);
